61323264e341a8e473a82cc69537ecfc746d3162
Hatice <haticekaraty@gmail.com>
2021-07-26 21:01:33 -0400

    commit tree eb2528f763f2999fae4bd99abb808774c53eedf8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+
+function mapToNegativize(sourceArray){
+    return sourceArray.map(el => el*(-1))
+};
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(el=>el)
+};
+function mapToDouble(sourceArray){
+    return sourceArray.map(el => el*2)
+};
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(el => el**2)
+};
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    return startingPoint + sourceArray.reduce(function(total,currentValue){
+        total = total + currentValue
+        return total
+    })
+};
+
+function reduceToAllTrue(sourceArray){
+    return sourceArray.reduce(function(accumulator,currentValue){
+        accumulator = !!accumulator && !!currentValue
+        return accumulator;
+    })
+};
+
+function reduceToAnyTrue(sourceArray){
+    return sourceArray.reduce(function(accumulator,currentValue){
+        accumulator = !!accumulator || !!currentValue
+        return accumulator;
+    })
+}
+//reduceToAnyTrue([1, 2, true, "razmatazz", false])
